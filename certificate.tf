f22ab2a8751f29eeb6aa468beab0b29c0457ffaf
daniel.schroeder <daniel.schroeder@groupon.com>
2019-11-15 12:31:25 +0100

    no longer creates wildcard certificate

--- certificate.tf
+++ certificate.tf
@@ -1,5 +1,7 @@
 resource "aws_acm_certificate" "certificate" {
-  domain_name               = "*.${local.root_domain_name}"
-  validation_method         = "EMAIL"
-  subject_alternative_names = [local.root_domain_name]
+  domain_name       = var.domain_name
+  validation_method = "EMAIL"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
--- locals.tf
+++ locals.tf
@@ -1,14 +1,5 @@
 locals {
   clean_domain_name = replace(var.domain_name, "/\\./", "_")
-  split_domain_name = split(".", var.domain_name)
-  root_domain_name = join(
-    ".",
-    slice(
-      local.split_domain_name,
-      1,
-      length(local.split_domain_name)
-    )
-  )
   default_filetypes = { // Only files types in this list will be synced to S3
 
     // common web documents
