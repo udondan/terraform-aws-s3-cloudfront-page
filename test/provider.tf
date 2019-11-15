c37aeef7b8b248daf18a6eda853f6d773ba149d8
daniel.schroeder <daniel.schroeder@groupon.com>
2019-11-15 12:17:22 +0100

    adding mock credentials for aws provider

--- test/provider.tf
+++ test/provider.tf
@@ -1,6 +1,12 @@
 provider "aws" {
-  region  = "us-east-1"
-  version = "2.26.0"
+  region                      = "us-east-1"
+  version                     = "2.26.0"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 provider "template" {
