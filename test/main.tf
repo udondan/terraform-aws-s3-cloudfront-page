1f5b2587098604ca8a71164817f9755c942d63e5
daniel.schroeder <daniel.schroeder@groupon.com>
2019-11-15 12:11:09 +0100

    fix invalid version contraint

--- test/main.tf
+++ test/main.tf
@@ -1,6 +1,5 @@
 module "s3-cloudfront-page" {
   source       = ".."
-  version      = "0.1.0"
   domain_name  = "www.example.com"
   root         = "${path.module}/public"
   filter_paths = ".*/font-awesome/(sprites|svgs)/.*"
